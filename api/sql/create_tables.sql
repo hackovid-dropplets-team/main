ce889c7a3215e2e0af6c435fae29269f5d9b5506
Arnau Comas Codina <comas1992@gmail.com>
2020-04-09 16:55:39 +0200

    [feat] sql

--- api/sql/create_tables.sql
+++ api/sql/create_tables.sql
@@ -9,12 +9,11 @@ BEGIN;
 CREATE TABLE users (
 	id SERIAL NOT NULL PRIMARY KEY,
 	is_admin BOOLEAN DEFAULT false,
-	center_id INTEGER,
 	username VARCHAR (200) UNIQUE NOT NULL,
-	firstname VARCHAR (200) NOT NULL,
-	lastname VARCHAR (200) NOT NULL,
+	firstname VARCHAR (200),
+	lastname VARCHAR (200),
 	password VARCHAR (200) NOT NULL,
-	email VARCHAR (200) UNIQUE NOT NULL,
+	email VARCHAR (200) UNIQUE,
 	phone VARCHAR (200),
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -27,21 +26,19 @@ CREATE TABLE users (
 --
 CREATE TABLE tickets (
 	id SERIAL NOT NULL PRIMARY KEY,
-	provider_id INTEGER NOT NULL,
 	user_id INTEGER,
-	center_id INTEGER,
-	barcode VARCHAR (200) UNIQUE,
-	wildcard VARCHAR (200) NULL,
+	title VARCHAR (200) UNIQUE,
+	description VARCHAR (200) NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	coordinates POINT NOT NULL,
 	disabled BOOLEAN DEFAULT false
 );
 
 COMMIT;
 
 -- DEFINE FOREIGN KEYS
-ALTER TABLE public.tickets ADD CONSTRAINT tickets_fk FOREIGN KEY (provider_id) REFERENCES public.providers(id);
-ALTER TABLE public.tickets ADD CONSTRAINT tickets_fk_1 FOREIGN KEY (user_id) REFERENCES public.users(id);
+ALTER TABLE public.tickets ADD CONSTRAINT tickets_fk FOREIGN KEY (user_id) REFERENCES public.users(id);
 
 -- TABLES FOR DISABLED ITEMS
 CREATE TABLE tickets_disabled (
--- api/sql/sample_data.sql
+++ api/sql/sample_data.sql
@@ -1,13 +1,13 @@
 SET ROLE 'dropplets_user';
 
-INSERT INTO public.users (role_id,username,firstname,lastname,password,email,phone,created_at,updated_at,last_login) VALUES
-(1,'acomas','Arnau','Comas Codina','0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5','comas1992@gmail.com','662284138','2020-04-07 17:14:33.978',NULL,NULL)
-,(1,'prova','Prova','Prova','0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5','prova@prova.com','','2020-04-07 19:09:14.807',NULL,NULL)
-,(1,'nom.cognoms','Nom','Cognoms','0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5','test@test.com','','2020-04-07 19:10:02.017',NULL,NULL)
+INSERT INTO public.users (is_admin,username,firstname,lastname,password,email,phone,created_at,updated_at,last_login) VALUES
+(true,'acomas',NULL,NULL,'0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5','comas1992@gmail.com','662284138','2020-04-07 17:14:33.978',NULL,NULL)
+,(false,'prova',NULL,NULL,'0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5','prova@prova.com','','2020-04-07 19:09:14.807',NULL,NULL)
+,(false,'nom.cognoms',NULL,NULL,'0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5','test@test.com','','2020-04-07 19:10:02.017',NULL,NULL)
 ;
 
---
--- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: polls
---
-
--- SELECT pg_catalog.setval('user_id_seq', 1, true);
+INSERT INTO public.tickets (user_id,title,description,coordinates) VALUES
+(1,'Tasca 1','Descripció 1',point(41.864049, 1.978700))
+,(1,'Tasca 2','Descripció 2',point(41.864760, 1.970868))
+,(1,'Tasca 3','Descripció 3',point(41.867812, 1.943327))
+;
